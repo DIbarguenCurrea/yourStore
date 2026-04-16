70c46ac96a8d7377b2bf1591450eb4a4b41dc089
Diego Sebastian <116876028+DIbarguenCurrea@users.noreply.github.com>
2026-04-16 23:34:27 +0200

    /health

--- SERVER/src/server.js
+++ SERVER/src/server.js
@@ -13,8 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend funcionando correctamente");
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Backend funcionando correctamente",
+  });
 });
 
 // Rutas de mis endpoints
